24ecbccdb702721261d75c0ad1cb9fb689d7513e
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-03-06 13:59:52 +0300

    Revert "указал явный путь к brew"
    This reverts commit 0d8b2a9b2f08973f28db5a0cd1b3cd1286b4e0dc.

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -22,7 +22,7 @@ local config = {
 
     font = wezterm.font("FiraCodeNerdFontMono"),
     front_end = 'OpenGL',
-    default_prog = { '/home/linuxbrew/.linuxbrew/bin/fish' },
+    default_prog = { 'fish' },
     scrollback_lines = 50000,
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
